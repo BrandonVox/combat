7ec2d89a3e13ecc536dc213aaa00e51005c3a68c
brandonvox <brandonvox9@gmail.com>
2022-11-02 10:52:55 +0700

    Initial files

---
+++ Source/Combat/CombatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBAT_API ACombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
